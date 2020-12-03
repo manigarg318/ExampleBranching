c6861e4623c8a5bbed4208a8dd6c7be209ba9428
ManiGarg <manigarg317@gmail.com>
2020-12-03 16:05:08 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppExerciseOnbranching.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string EDesgination { get; set; }
+        public DateTime EDoj { get; set; }
+
+
+
+    }
+}
